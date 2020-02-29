b753c04cd5af1d55bb3509413612d4d33f0cdb3e
rahul250892 <rahulkumar250892@outlook.com>
2020-02-29 13:04:02 +0530

    Updates

---
+++ precache-manifest.05b6d420dc2c3c15d8b700ce98dd62c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/shop-cart-app/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/shop-cart-app/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/shop-cart-app/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/shop-cart-app/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/shop-cart-app/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "6820484fec1e1387390c",
+    "url": "/shop-cart-app/static/js/runtime~main.6820484f.js"
+  },
+  {
+    "revision": "211af3cf8adcf89276a4",
+    "url": "/shop-cart-app/static/js/main.211af3cf.chunk.js"
+  },
+  {
+    "revision": "610752b7a16853a9d977",
+    "url": "/shop-cart-app/static/js/1.610752b7.chunk.js"
+  },
+  {
+    "revision": "211af3cf8adcf89276a4",
+    "url": "/shop-cart-app/static/css/main.5c4af593.chunk.css"
+  },
+  {
+    "revision": "ebd4b60577bcbb93ed4b07194a341faa",
+    "url": "/shop-cart-app/index.html"
+  }
+];
